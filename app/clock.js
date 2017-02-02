d809903e9ab5176efef4b1fd7b3d4611233cf7d8
Sergio Rojas <serg.rojas@tutamail.com>
2017-02-02 12:23:11 -0800

    Add more function to clock

--- app/clock.js
+++ app/clock.js
@@ -18,8 +18,36 @@ var Clock = function(minutes) {
     this.minutes--;
   };
   this.countdown = function() {
+    var clock = this;
+    var minutes = clock.minutes - 1;
+    var seconds = clock.seconds;
 
+    if(!clock.paused) {
+
+      var countDown = setInterval(function() {
+        if(minutes === 0 && seconds < 1) {
+          clock.togglePaused();
+          clearInterval(countDown);
+        }
+        if(minutes <= 1) {
+          minutes = 0;
+        }
+        if(seconds < 1) {
+          minutes--;
+          seconds = 60;
+        }
+
+        seconds--;
+
+        console.log(minutes + ":" + seconds);;
+      }, 1000);
+    }
   };
 };
 
+var session = new Clock(25);
+var breaks = new Clock(5);
+var p = new Clock(1);
+p.togglePaused();
+p.countdown();
 module.exports = Clock;
